91cfe0675848d81bf4d09132c5f8640a68ec4da7
Ernest Aboah <ernesco28@gmail.com>
2025-06-16 00:46:43 +0000

    set up light and dark theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ const Home = () => {
   return (
     <div>
       <h1 className="h1-bold"> Welcome Ernest </h1>
-      <h1 className="h1-bold font-space-grotesk"> Welcome Aboah </h1>
     </div>
   );
 };
