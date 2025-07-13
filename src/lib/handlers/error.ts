05e3786062c6e4c2e880b7231ed958657dd9318b
Ernest Aboah <ernesco28@gmail.com>
2025-07-13 02:26:15 +0000

    Implement logging

--- src/lib/handlers/error.ts
+++ src/lib/handlers/error.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { RequestError, ValidationError } from "../http-errors";
 import { ZodError } from "zod";
+import logger from "../logger";
 
 export type ResponseType = "api" | "server";
 
@@ -28,6 +29,11 @@ const formatResponse = (
 
 const handleError = (error: unknown, responseType: ResponseType = "server") => {
   if (error instanceof RequestError) {
+    logger.error(
+      { err: error },
+      `${responseType.toUpperCase()} Error: ${error.message}`
+    );
+
     return formatResponse(
       responseType,
       error.statusCode,
@@ -41,6 +47,11 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
       error.flatten().fieldErrors as Record<string, string[]>
     );
 
+    logger.error(
+      { err: error },
+      `Validation Error: ${validationError.message}`
+    );
+
     return formatResponse(
       responseType,
       validationError.statusCode,
@@ -50,8 +61,11 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
   }
 
   if (error instanceof Error) {
+    logger.error(error.message);
     return formatResponse(responseType, 500, error.message);
   }
+
+  logger.error({ err: error }, "An unexpected error occurred.");
 
   return formatResponse(responseType, 500, "An unexpected error occurred.");
 };
